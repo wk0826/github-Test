c5035093a8341bcc60af6a504e21ceaa47c320fc
”wkgit config --global user.name ”wk <wk@git.com>
2023-08-15 17:56:31 +0800

    githubTest

---
+++ src/main/java/com/cxk/java/GithubTest.java
@@ -0,0 +1,20 @@
+package com.cxk.java;
+
+/**
+ * @ fileName:GithubTest
+ * @ description:
+ * @ author:wk
+ * @ createTime:2023/8/15 17:52
+ * @ version:1.0.0
+ */
+
+public class GithubTest {
+    public static void main(String[] args) {
+        System.out.println("20030826");
+        System.out.println("20030826");
+        System.out.println("20030826");
+        System.out.println("20030826");
+        System.out.println("20030826");
+        System.out.println("20030826");
+    }
+}
